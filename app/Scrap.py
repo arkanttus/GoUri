2047af5e1a6953622b757fb2658543e031b22f44
arkanttus <italo.oliveira300@hotmail.com>
2019-12-01 18:56:50 -0500

    Create Scrap

---
+++ app/Scrap.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup as Bs
+from flask import make_response, jsonify
+
+def scrap(userId):
+    url = "https://www.urionlinejudge.com.br/judge/pt/profile/" + userId
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    response=requests.get(url, headers=headers)
+
+    soup = Bs(response.text, 'html.parser')
+
+    pages = soup.find('div', id='table-info')
+    pages = pages.text.split(' ')
+    pages = int(pages[-1])
+
+    tds = soup.find_all('td', class_='id')
+
+    questions = set()
+
+    for td in tds:
+        link = td.find('a')
+        questions.add(link.text)
+
+    url += '?page='
+
+    for page in range(2,pages):
+        print(page)
+        response = requests.get(url + str(page), headers=headers)
+        soup = Bs(response.text, 'html.parser')
+
+        tds = soup.find_all('td', class_='id')
+
+        for td in tds:
+            link = td.find('a')
+            questions.add(link.text)
+
+    response = {'questions': list(questions)}
+    print(response)
+
+    return jsonify(response)
+
+def teste():
+    c = {'1', '2', '3'}
+    #b = list(c)
+    a = {'a': list(c)}
+    return jsonify(a)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, make_response, jsonify
+from ..app import Scrap
 
 routes = Blueprint('routes', __name__)
 
-@routes.route('/<username>')
-def teste(username):
-    response = {'koe': 'teste', 'user': username}
-    return jsonify(response)
+@routes.route('/')
+def teste():
+   return Scrap.teste()
+
+@routes.route('/scrap/<userId>', methods=['GET'])
+def scrap(userId):
+    return Scrap.scrap(userId)
